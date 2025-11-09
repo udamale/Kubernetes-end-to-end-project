d4ab68cfe4cb4cdc1cf100a49b8b01c8877d1939
Ganesh udamale <148082412+udamale@users.noreply.github.com>
2025-11-09 15:14:15 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#         source = "hashicorp/aws"
+#         version = ">= 5.25.0"
+#     }
+#   }
 
-  backend "s3" {
-    bucket = "state-remote-storeganesh"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
-  }
+#   backend "s3" {
+#     bucket = "state-remote-storeganesh"
+#     key    = "eks/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
 
-  required_version = ">= 1.6.3"
-}
+#   required_version = ">= 1.6.3"
+# }
